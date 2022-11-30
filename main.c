60e1a99d25fcaaf390c72add042a953779f68738
andre <andreaferraboli2004@gmail.com>
2022-11-30 21:24:27 +0100

    update

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "string.h"
 typedef struct {
     char **f;
     int n;
@@ -13,10 +13,10 @@ int carica(char *nf, testo *txt);
 int main(int argc, char *argv[]) {
     testo temp;
     char *str[2] = {"pippo9", "pluto"};
-    temp.f = str[0];
+    temp.f = str;
     temp.n = 2;
 //    mostra(temp);
-    carica("text.txt", &temp);
+    carica(argv[1], &temp);
     mostra(temp);
     return 0;
 }
@@ -24,30 +24,31 @@ int main(int argc, char *argv[]) {
 void mostra(testo text) {
     int i;
     for (i = 0; i < text.n; ++i) {
-        printf("%s", text.f[i]);
+        printf("stringa[i]=%s", text.f[i]);
     }
 }
 
 int carica(char *nf, testo *txt) {
-    printf("file:%s", nf);
+    printf("file:%s \n", nf);
     FILE *fp;
-    if ((fp = fopen("text.txt", "r")) == NULL) {
+    if ((fp = fopen(nf, "r")) == NULL) {
         printf("Errore nell’apertura del file!");
         return -1;
     }
-    int n_righe, i;
-    fscanf(fp, "%d", &n_righe);
+    int n_righe, i=0;
+    fscanf(fp, "%d%*c", &n_righe);
     printf("n_righe=%d \n",n_righe);
     if (n_righe <= 0)
         return -2;
     char **f = (char **) malloc(BUFSIZ * n_righe);
     char *res;
-    for (i = 0; i < n_righe; ++i) {
-//        fscanf(fp, "%s", f[i]);
-        fgets(res, BUFSIZ, fp);
-        if( res==NULL )
-            break;
-        printf("%s",f[i]);
+    while (!feof(fp)) {
+        printf("i=%d",i);
+        fgets(res,100,fp);
+        char str[strlen(res)];
+        strcpy(str,res);
+        strcpy(f[i],str);
+        printf("f[i]=%s \n", f[i++]);
     }
     txt->f = f;
     txt->n = n_righe;
